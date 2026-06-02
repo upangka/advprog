c1b80bc850a7f3798b5e9196e438b9eb83017333
pkmer <q10viking@outlook.com>
2026-06-02 18:09:17 +0800

    delete manager

--- 04_actors/actors.py
+++ 04_actors/actors.py
@@ -56,9 +56,38 @@ def printer_example():
 
 printer_example()
 
+print('-'*20)
+
+# Question: Actors are always referenced by an address which
+# is a string such as 'printer' in this example.  Is there
+# a mechanism for an actor to obtain it's own address?
+
+"""Exercise 02 Understanding the Manager
+
+The purpose of the `Manager` class is to create a managed
+environment for the `Actor` instances and to handle all of the
+associated messaging.  Actors are always associated with an
+enclosing Manager.  When the Manager goes away, the Actors contained
+within it should also go away.  Verify that this seems to happen by
+trying this example:
+"""
 
 
+def manager_example():
+    m = Manager()
+    # create a few actors
+    m.spawn('广州', Printer())
+    m.spawn('深圳', Printer())
+    # send a few messages
+    m.send(Message(source="广州",dest="深圳",content="Hi 深圳"))
+    m.send(Message(source="深圳",dest="广州",content="Hi 广州"))
+    # delete the manager
+    # this should produce two messages about Printer actor going away
+    print("About to delete manager")
+    del m
+    print("Manager deleted")
 
+manager_example()
 
 
 
