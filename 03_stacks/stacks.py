ebe4b1286960870a2104ddfa5aee5c81d7c7044e
pkmer <q10viking@outlook.com>
2026-06-02 16:22:02 +0800

    exercise 07

--- 03_stacks/stacks.py
+++ 03_stacks/stacks.py
@@ -114,7 +114,7 @@ def test_calculator(calc):
     calc.push(5)
     calc.div()
     assert calc.pop() == 2.0
-    print("Good Calculator ")
+    print(f"Good {type(calc).__name__}")
 
 
 calc = Calculator()
@@ -314,7 +314,13 @@ def add_stack_checking(cls):
     return cls
 
 
+@add_stack_debug
+@add_stack_checking
+class MyCalculator(Calculator):
+    pass
 
+calc = MyCalculator()
+test_calculator(calc)
 
 
 """Exercise 07 Only a Calculator
@@ -327,8 +333,157 @@ and so forth.  Your class should pass the tests below.
 """
 
 
+import math
+
+class Calculator:
+
+    def __init__(self):
+        self._values = []
+
+    def push(self,item):
+        if not isinstance(item,(int,float)):
+            raise TypeError("Only accept number")
+
+        self._values.append(item)
+
+    def pop(self):
+        return self._values.pop()
+
+    def _pop2(self):
+        if len(self._values) < 2:
+            raise NotEnoughValues("Not enough values")
+
+        return (self.pop(),self.pop())
+
+    def add(self):
+        right,left = self._pop2()
+        self.push(right + left)
+
+    def sub(self):
+        right,left = self._pop2()
+        self.push(left - right)
+
+    def mul(self):
+        right,left = self._pop2()
+        self.push(right * left)
+
+    def div(self):
+        right,left = self._pop2()
+        self.push(left / right)
+
+    def pow(self):
+        exp,base = self._pop2()
+        self.push(base ** exp)
+
+    def sqrt(self):
+        if len(self._values) < 1:
+            raise NotEnoughValues()
+        self.push(math.sqrt(self.pop()))
+        
+    def swap(self):
+        right,left = self._pop2()
+        self.push(right)
+        self.push(left)
 
 
+    # exercise 08
+    def run(self,instructions):
+        pass
 
+
+def test_calculator(calc):
+
+    calc.push(23)
+    calc.push(45)
+    calc.add()
+    assert calc.pop() == 68
+
+    calc.push(2)
+    calc.push(3)
+    calc.push(4)
+    calc.add()
+    calc.mul()
+    assert calc.pop() == 14
+
+    calc.push(10)
+    calc.push(3)
+    calc.sub()
+    assert calc.pop() == 7
+
+    calc.push(10)
+    calc.push(5)
+    calc.div()
+    assert calc.pop() == 2.0
+
+    calc.push(10)
+    calc.push(2)
+    calc.pow()
+    assert calc.pop() == 100
+
+    calc.push(100)
+    calc.sqrt()
+    assert calc.pop() == 10.0
+
+    calc.push(2)
+    calc.push(3)
+    calc.swap()
+    assert calc.pop() == 2
+    assert calc.pop() == 3
+
+    # make sure that only numeric values can be pushed
+    try:
+        calc.push("two")
+    except TypeError as err:
+        pass
+    else:
+        assert False, "Bad Calculator!"
+    print("Good Calculator")
+
+test_calculator(Calculator())
+
+
+"""Exercise 08 The Script
+
+Mel wants to know if common calculations can be "scripted" or memorized in
+some way.  For example, a common task in math class is to compute
+the length of the hypotenuse of a triangle.
+
+      |\
+      | \
+      |  \
+      |   \
+  x   |    \   hypot = sqrt(x**2 + y**2)
+      |     \
+      |      \
+      |       \
+      |________\
+          y
+
+Mel has written out a list of "instructions" that carry out this
+operation, assuming that the values of "x" and "y" have already been
+entered.  Could you give the `Calculator` class a "run" method that
+executes the instructions one after the other?  That is your task.
+"""
+
+
+hypot = [
+    ('push',2),
+    ('pow',),
+    ('swap',),
+    ('push',2),
+    ('pow',),
+    ('add',),
+    ('sqrt',)
+]
+
+
+def test_hypot():
+    calc = Calculator()
+    calc.push(3)
+    calc.push(4)
+
+    calc.run(hypot)
+    assert calc.pop() == 5.0
+    print("Good Script!")
 
 
