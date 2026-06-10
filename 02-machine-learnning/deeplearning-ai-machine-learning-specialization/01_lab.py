3e65b93a611c1636ca714d1e9087a5c180a6c510
pkmer <q10viking@outlook.com>
2026-06-10 22:57:32 +0800

    init project

---
+++ 02-machine-learnning/deeplearning-ai-machine-learning-specialization/01_lab.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+# Get the directory where this script is located
+script_dir = Path(__file__).parent
+plt.style.use(script_dir / 'deeplearning.mplstyle')
+
+x_train = np.array([1.0, 2.0])
+y_train = np.array([300.0, 500.0])
+
+# Plot the data points
+plt.scatter(x_train, y_train, marker='x', c='r')
+# Set the title
+plt.title("Housing Prices")
+# Set the y-axis label
+plt.ylabel('Price (in 1000s of dollars)')
+# Set the x-axis label
+plt.xlabel('Size (1000 sqft)')
+plt.show()
---
+++ 02-machine-learnning/deeplearning-ai-machine-learning-specialization/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from deeplearning-ai-machine-learning-specialization!")
+
+
+if __name__ == "__main__":
+    main()
