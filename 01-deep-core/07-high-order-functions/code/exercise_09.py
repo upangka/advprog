a980ef053e69b9b94e02dc11963ba0de73c8ba9e
pkmer <q10viking@outlook.com>
2026-06-16 17:16:39 +0800

    finished zero or more

---
+++ 01-deep-core/07-high-order-functions/code/exercise_09.py
@@ -0,0 +1,28 @@
+from exercise_08 import parse_setting
+
+
+def zero_or_more(parser):
+    def parse(text, index):
+        results = []
+
+        while m := parser(text, index):
+            value, index = m
+            results.append(value)
+        return (results, index)
+
+    return parse
+
+
+parse_settings = zero_or_more(parse_setting)
+
+
+def test_parse_settings():
+    assert parse_settings("speed=42;size=9.5;maxspeed=1000;", 0) == (
+        [("speed", 42), ("size", 9.5), ("maxspeed", 1000)],
+        32,
+    )
+    assert parse_settings("", 0) == ([], 0)
+    print("Good tests for test_parse_settings")
+
+
+test_parse_settings()
