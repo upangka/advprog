bcdbff295681f0e83140675441ac6751d6e8ae99
pkmer <q10viking@outlook.com>
2026-06-16 22:53:45 +0800

    work in progress

--- 01-deep-core/07-high-order-functions/code/interfacee/exercise_03.py
+++ 01-deep-core/07-high-order-functions/code/interfacee/exercise_03.py
@@ -25,8 +25,10 @@ after_1(1, lambda: add(2, 3))
 after_1(1, lambda: add(x=2, y=3))
 
 after_2(1, add, (2, 3))
+after_2(1, add, kwargs={'x':2, 'y':3})
+# Commentary: I might be inclined to write it like this so I could
+# keep the syntax of kwargs.
 after_2(1, add, kwargs=dict(x=2, y=3))
-after_2(1, add, kwargs={"x": 2, "y": 3})
 
 after_3(1, add, 2, 3)
 after_3(1, add, x=2, y=3)
