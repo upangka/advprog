0c3cbbe15b53c6e4ece370c6aa97a0c40b4a89f2
pkmer <q10viking@outlook.com>
2026-06-20 15:59:23 +0800

    ready for exercise 03

---
+++ 01-deep-core/08-network/code/protocol/exercise_03.py
@@ -0,0 +1,11 @@
+from exercise_01 import recreate_message
+from protocol import ChatMessage, Message, PlayerUpdate
+from exercise_02 import read_line,receive_exactly,receive_message
+
+
+def test_receive_message():
+    ...
+
+
+if __name__ == "__main__":
+    test_receive_message()
