e3d969ad1b598fabc00f8a51bd8800718320513c
pkmer <q10viking@outlook.com>
2026-06-23 20:58:33 +0800

    work in exercise06

--- 01-deep-core/01-stdtypes/code/fractions/exercise_06.py
+++ 01-deep-core/01-stdtypes/code/fractions/exercise_06.py
@@ -8,6 +8,18 @@ def gcd(a, b):
     return a
 
 
+def make_frac(numerator, denominator):
+    return Fraction(numerator, denominator)
+
+
+def numerator(f):
+    return f.numerator
+
+
+def denominator(f):
+    return f.denominator
+
+
 # We will define a proper class
 class Fraction:
     def __init__(self, numerator, denominator):
@@ -41,3 +53,48 @@ class Fraction:
 
     def __repr__(self) -> str:
         return f"Fraction(numerator={self.numerator},denominator={self.denominator})"
+
+
+def add_frac(a, b):
+    return a + b
+
+
+def sub_frac(a, b):
+    return a - b
+
+
+def mul_frac(a, b):
+    return a * b
+
+
+def div_frac(a, b):
+    return a / b
+
+
+def test_frac():
+    a = make_frac(4, 6)
+    assert (numerator(a), denominator(a)) == (2, 3)
+
+    b = make_frac(-3, -4)
+    assert (numerator(b), denominator(b)) == (3, 4)
+
+    c = make_frac(3, -4)
+    assert (numerator(c), denominator(c)) == (-3, 4)
+
+    d = add_frac(a, b)
+    assert (numerator(d), denominator(d)) == (17, 12)
+
+    e = sub_frac(a, b)
+    assert (numerator(e), denominator(e)) == (-1, 12)
+
+    f = mul_frac(a, b)
+    assert (numerator(f), denominator(f)) == (1, 2)
+
+    g = div_frac(a, b)
+    assert (numerator(g), denominator(g)) == (8, 9)
+
+    print("Good fractions In old version")
+
+
+if __name__ == "__main__":
+    test_frac()
