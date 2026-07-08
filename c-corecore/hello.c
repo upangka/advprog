fc75df4a0a9e61861d650ff9bb4f5fd4313575e6
pkmer <q10viking@outlook.com>
2026-07-08 19:04:00 +0800

    add file

--- c-corecore/hello.c
+++ c-corecore/hello.c
@@ -2,6 +2,8 @@
 
 int main()
 {
+    printf("Exchange Data\n");
     printf("Hello,World\n");
     return 0;
+
 }
---
+++ c-corecore/input.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	printf("Enter two integers\n");
+
+	int num1,num2,sum;
+	scanf("%d %d",&num1,&num2);
+	sum = num1 + num2;
+
+	printf("%d",sum);
+	return 0;
+}
