9c8ea8d180b2ce1aa9b9c74129fbce1527a34e65
pkmer <q10viking@outlook.com>
2026-07-14 23:49:21 +0800

    finished rand

---
+++ c-corecore/libs/random/code/exercise_01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    time_t t;
+    // 复制给t并返回一模一样的值
+    printf("%ld\n",time(&t));
+    printf("%ld\n",t); 
+
+    // srand(time(&t));
+    srand(t);
+    for(int i=0;i<5;i++){
+        printf("%d\n",rand() % 21);
+    }
+    
+    return 0;
+}
+/*output
+1784044120
+1784044120
+15
+14
+20
+20
+18
+*/
