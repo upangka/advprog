3ac5f43036140a5f22d67707cb09b65ed6736a8c
pkmer <q10viking@outlook.com>
2026-07-15 10:15:39 +0800

    继续

--- c-corecore/arrays/code/exercise_02.c
+++ c-corecore/arrays/code/exercise_02.c
@@ -1,3 +1,7 @@
+/*
+编写程序，生成一种贯穿10×10字符数组（初始时全为字符'.'）的“随机步法”​。程序必须随机地从一个元素“走到”另一个元素，每次都向上、向下、向左或向右移动一个元素位置。已访问过的元素按访问顺序用字母A到Z进行标记。
+
+ */
 #include <stdio.h>
 #define BOARD_HEIGHT 10
 #define BOARD_WIDTH 10
@@ -24,7 +28,8 @@ void init_arr(char arr[][BOARD_WIDTH],char init_val){
 int main(){
     char board[10][10];
     init_arr(board,'.');
-    board[5][5] = 'A';
+    
+
     print_arr(board);
     return 0;
 }
