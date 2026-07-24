347c6f43813f0b0b9eb7e182201a1f6f1f13fc65
pkmer <q10viking@outlook.com>
2026-07-24 15:02:03 +0800

    update loop

--- ts-core/node-cc/streams/code/exercise_10.ts
+++ ts-core/node-cc/streams/code/exercise_10.ts
@@ -3,24 +3,24 @@ import { Buffer } from "node:buffer";
 
 const srcFile = await fs.open("./dist/test.txt", "r");
 const desFile = await fs.open("./dist/copy.txt", "w");
-let bytesRead = 0;
-do {
+let bytesRead = -1;
+while (bytesRead !== 0) {
   // 16384 也是默认值
   const result = await srcFile.read({ length: 16384 });
   bytesRead = result.bytesRead;
 
-  // 没有数据直接退出
-  if (bytesRead == 0) break;
-
-  // 处理未装满的
-  if (bytesRead < 16384) {
+  if (bytesRead == 0) {
+    // 没有数据直接退出
+    break;
+  } else if (bytesRead < 16384) {
+    // 处理未装满的
     const newBuffer = Buffer.alloc(bytesRead);
     result.buffer.copy(newBuffer, 0, 0, bytesRead);
     await desFile.write(newBuffer);
   } else {
     await desFile.write(result.buffer);
   }
-} while (bytesRead > 0);
+}
 
 const srcStat = await srcFile.stat();
 const desStat = await desFile.stat();
