eae7c3450da0b86ebcc155c9598d69df2a0158da
pkmer <q10viking@outlook.com>
2026-07-29 09:24:17 +0800

    finihsed

--- modern-java/Jbang.md
+++ modern-java/Jbang.md
@@ -22,3 +22,16 @@ j! --java 25 hello.java
 ```sh
 ./xxx.java
 ```
+
+## 文件顶部配置
+
+注意没有空格
+
+```java
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 25+
+//DEPS org.projectlombok:lombok:1.18.46
+//DEPS tools.jackson.core:jackson-databind:3.2.1
+//REPOS aliyun=https://maven.aliyun.com/repository/central
+//JAVAC_OPTIONS -proc:full
+```
--- modern-java/vavrtuple.java
+++ modern-java/vavrtuple.java
@@ -1,3 +1,4 @@
+
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 25+
 //DEPS io.vavr:vavr:1.0.0
@@ -7,12 +8,12 @@ void main(String... args) {
     IO.println("Exchange data");
     var langOne = "Java";
     var langTwo = "Python";
-    IO.println("langOne=%s langTwo=%s".formatted(langOne,langTwo));
-    
-    // Exchange data 
-    var tuple = Tuple.of(langOne,langTwo);
+    IO.println("langOne=%s langTwo=%s".formatted(langOne, langTwo));
+
+    // Exchange data
+    var tuple = Tuple.of(langOne, langTwo);
     langOne = tuple._2;
     langTwo = tuple._1;
-    IO.println("langOne=%s langTwo=%s".formatted(langOne,langTwo));
-    
+    IO.println("langOne=%s langTwo=%s".formatted(langOne, langTwo));
+
 }
