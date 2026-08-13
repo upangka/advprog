ebb492cb32b4eb64b63810b8f1f84bd511ef3e1e
pkmer <q10viking@outlook.com>
2026-08-13 21:50:28 +0800

    create project structue

---
+++ modern-java/concurrency-programming/simple-server/code/command/Command.java
@@ -0,0 +1,13 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 25+
+package command;
+
+public abstract class Command{
+    protected String[] commands;
+
+    public Command(String[] commands){
+        this.commands = commands;
+    }
+
+    public abstract String execute();
+}
---
+++ modern-java/concurrency-programming/simple-server/code/main.java
@@ -0,0 +1,6 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 25+
+
+void main(String... args) {
+    IO.println("Hello World");
+}
