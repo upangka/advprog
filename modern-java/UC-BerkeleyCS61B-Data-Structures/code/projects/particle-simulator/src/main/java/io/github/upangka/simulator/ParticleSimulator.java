e13320604c3d2ba66bcca0882d089de31e3e6861
upangka <q10viking@outlook.com>
2026-08-17 20:18:35 +0800

    优化Simulator，添加获取邻居的方法，并且抽离验证坐标

--- modern-java/UC-BerkeleyCS61B-Data-Structures/code/projects/particle-simulator/src/main/java/io/github/upangka/simulator/ParticleSimulator.java
+++ modern-java/UC-BerkeleyCS61B-Data-Structures/code/projects/particle-simulator/src/main/java/io/github/upangka/simulator/ParticleSimulator.java
@@ -4,6 +4,9 @@ import edu.princeton.cs.algs4.StdDraw;
 import lombok.Getter;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
 
 /**
@@ -12,54 +15,83 @@ import java.util.function.BiConsumer;
  * @version 1.0
  * @since 2026/8/17
  */
+@Getter
 public class ParticleSimulator {
-    @Getter
     private final int width;
-    @Getter
     private final int height;
     private final Particle[][] particles;
 
 
-    public ParticleSimulator(int w,int h){
+    public ParticleSimulator(int w, int h) {
         this.width = w;
         this.height = h;
         this.particles = new Particle[w][h];
 
-        traverseParticles((x,y) -> particles[x][y] = new Particle(ParticleFlavor.EMPTY));
+        traverseParticles((x, y) -> particles[x][y] = new Particle(ParticleFlavor.EMPTY));
     }
 
 
-    public void drawParticles(){
+    public void drawParticles() {
 
-        traverseParticles((x,y) -> {
+        traverseParticles((x, y) -> {
             var particle = particles[x][y];
             StdDraw.setPenColor(particle.color());
             // 以半边长为0.5,正方形的中心画一个正方形
-            StdDraw.filledSquare(x + 0.5d, y + 0.5d,0.5d);
+            StdDraw.filledSquare(x + 0.5d, y + 0.5d, 0.5d);
         });
     }
 
 
-    public void traverseParticles(BiConsumer<Integer,Integer> consumer){
+    public void traverseParticles(BiConsumer<Integer, Integer> consumer) {
         for (int x = 0; x < particles.length; x++) {
             for (int y = 0; y < particles[0].length; y++) {
-               consumer.accept(x,y);
+                consumer.accept(x, y);
             }
         }
 
     }
 
-    public void changeParticleFlavor(int x,int y,ParticleFlavor flavor){
-        if(x >= 0 && x < width && y >= 0 && y < height){
-            var particle =  particles[x][y];
-            if(particle != null){
+    public void changeParticleFlavor(int x, int y, ParticleFlavor flavor) {
+        if (validPosition(x, y)) {
+            var particle = particles[x][y];
+            if (particle != null) {
                 particle.changeFlavor(flavor);
             }
         }
     }
+    /** 类似`毒丸` */
+    public static final Particle SENTINEL = new Particle(ParticleFlavor.BARRIER);
+    public Map<Direction, Particle> getNeighbors(int x, int y) {
+
+        var ret = new HashMap<Direction, Particle>();
+
+        Map<Direction, List<Integer>> directions = Map.of(
+                Direction.UP, List.of(x, y + 1),
+                Direction.DOWN, List.of(x, y - 1),
+                Direction.LEFT, List.of(x - 1, y),
+                Direction.RIGHT, List.of(x + 1, y));
+
+        directions.forEach((direction, position) -> {
+            int neighborX = position.get(0);
+            int neighborY = position.get(1);
+            if (validPosition(neighborX, neighborY)) {
+                ret.put(direction, particles[neighborX][neighborY]);
+            } else {
+                ret.put(direction, SENTINEL);
+            }
+        });
+
+        return ret;
+    }
+
+
+    private boolean validPosition(int x, int y) {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
+
 
     static void main() {
-        var ret = new ParticleSimulator(150,150);
+        var ret = new ParticleSimulator(150, 150);
         System.out.println(Arrays.toString(ret.particles));
     }
 
