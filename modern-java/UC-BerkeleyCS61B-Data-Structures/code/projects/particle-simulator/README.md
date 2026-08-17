4f24caea432dc50afedaf75dc33217aa8a5d3917
upangka <q10viking@outlook.com>
2026-08-17 15:27:30 +0800

    初始化项目

---
+++ modern-java/UC-BerkeleyCS61B-Data-Structures/code/projects/particle-simulator/README.md
@@ -0,0 +1,3 @@
+
+
+[algs4 java document](https://algs4.cs.princeton.edu/code/javadoc/edu/princeton/cs/algs4/package-summary.html)
---
+++ modern-java/UC-BerkeleyCS61B-Data-Structures/code/projects/particle-simulator/src/main/java/io/github/upangka/simulator/ParticleSimulator.java
@@ -0,0 +1,20 @@
+package io.github.upangka.simulator;
+
+import edu.princeton.cs.algs4.StdDraw;
+
+/**
+ *
+ * @author 鲨鱼不喝Jvaa 抖音号:77283340926
+ * @version 1.0
+ * @since 2026/8/17
+ */
+public class ParticleSimulator {
+
+    public static void main() {
+        StdDraw.setXscale(0, 150);
+        StdDraw.setYscale(0, 150);
+        StdDraw.enableDoubleBuffering();
+        StdDraw.clear(StdDraw.BLACK);
+        StdDraw.show();
+    }
+}
