eb2927feedc5b625a166b7a90fa9198978635ada
upangka <q10viking@outlook.com>
2026-08-26 15:51:55 +0800

    BSTMap - BSTNode inner class finished find and insert

--- modern-java/UC-BerkeleyCS61B-Data-Structures/code/week7/homework/bst-map/src/main/java/cs61b/BSTMap.java
+++ modern-java/UC-BerkeleyCS61B-Data-Structures/code/week7/homework/bst-map/src/main/java/cs61b/BSTMap.java
@@ -49,6 +49,34 @@ public class BSTMap<K extends Comparable<? super K>, V> implements Map16B<K, V> 
                 return find(bstNode.right,sk);
             }
         }
+
+        /**
+         * 插入节点
+         * @param bstNode 当前要查找的节点
+         * @param sk    Search Key
+         * @param val   Value
+         * @param <K>
+         * @param <V>
+         */
+        private static  <K extends Comparable<? super K>, V> BSTNode<K,V> insert(BSTNode<K,V> bstNode,K sk,V val){
+            // Always insert at leaf node
+            if(bstNode == null){
+                return new BSTNode<K,V>(sk,val);
+            }
+
+            var ret = bstNode.key.compareTo(sk);
+
+            if(ret < 0){
+                bstNode.right = insert(bstNode.right,sk,val);
+            }else if(ret > 0){
+                bstNode.left = insert(bstNode.left,sk,val);
+            }else{
+                // 相等，直接更新value
+                bstNode.value = val;
+            }
+
+            return bstNode;
+        }
     }
 
     private BSTNode<K,V> root;
@@ -83,7 +111,7 @@ public class BSTMap<K extends Comparable<? super K>, V> implements Map16B<K, V> 
      */
     @Override
     public void put(K key, V value) {
-
+        root = BSTNode.insert(root,key,value);
     }
 
 
@@ -96,7 +124,8 @@ public class BSTMap<K extends Comparable<? super K>, V> implements Map16B<K, V> 
      */
     @Override
     public boolean containsKey(K key) {
-        return false;
+        var ret =  BSTNode.find(root, key);
+        return ret != null;
     }
 
     /**
