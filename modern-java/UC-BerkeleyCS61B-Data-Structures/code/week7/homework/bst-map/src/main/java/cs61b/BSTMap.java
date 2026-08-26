c1b850154f85ca832422eaac864940ab90630262
upangka <q10viking@outlook.com>
2026-08-26 18:22:14 +0800

    完成删除

--- modern-java/UC-BerkeleyCS61B-Data-Structures/code/week7/homework/bst-map/src/main/java/cs61b/BSTMap.java
+++ modern-java/UC-BerkeleyCS61B-Data-Structures/code/week7/homework/bst-map/src/main/java/cs61b/BSTMap.java
@@ -1,6 +1,8 @@
 package cs61b;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  *
@@ -51,6 +53,7 @@ public class BSTMap<K extends Comparable<? super K>, V> implements Map16B<K, V> 
             }
         }
 
+
         /**
          * 插入节点
          *
@@ -78,6 +81,69 @@ public class BSTMap<K extends Comparable<? super K>, V> implements Map16B<K, V> 
             }
 
             return bstNode;
+        }
+
+        private record DeleteResult<K, V>(
+                /** 要链接的新节点 */
+                BSTNode<K, V> newNode,
+                /** 已经删除的节点 */
+                BSTNode<K, V> deletedNode) {
+        }
+
+        /**
+         * 删除操作
+         *
+         * @param bstNode 当前节点
+         * @param sk      search key
+         * @return 删除的节点
+         */
+        private static <K extends Comparable<? super K>, V> DeleteResult<K, V> delete(BSTNode<K, V> bstNode, K sk) {
+            if (bstNode == null) {
+                // 没有找到要删除的
+                return new DeleteResult<K, V>(bstNode, null);
+            }
+
+            var ret = bstNode.key.compareTo(sk);
+            DeleteResult<K, V> delRet = null;
+
+            if (ret < 0) {
+                delRet = delete(bstNode.right, sk);
+                bstNode.right = delRet.newNode;
+                // 重新包装，保持维持原先元素的链接
+                return new DeleteResult<>(bstNode, delRet.deletedNode);
+            } else if (ret > 0) {
+                delRet = delete(bstNode.left, sk);
+                bstNode.left = delRet.newNode;
+                // 重新包装，保持维持原先元素的链接
+                return new DeleteResult<>(bstNode, delRet.deletedNode);
+            } else {
+                // 没有子节点或者只有一个子节点
+                if (bstNode.left == null) {
+                    return new DeleteResult<K, V>(bstNode.right, bstNode);
+                }
+
+                if (bstNode.right == null) {
+                    return new DeleteResult<K, V>(bstNode.left, bstNode); // 只有一个left节点
+                }
+
+                // 处理两个节点，这里采用Hibbard deletion的前驱节点
+                var predecessor = bstNode.left;
+                while (predecessor.right != null) {
+                    predecessor = predecessor.right;
+                }
+
+                // 构建删除节点的信息
+                var delNode = new BSTNode<K, V>(bstNode.key, bstNode.value);
+
+                // trick: 直接更改 key和value,然后删除前驱节点，因为前驱节点只要一个或者0个子节点，
+                // 不会形成无限递归
+                bstNode.key = predecessor.key;
+                bstNode.value = predecessor.value;
+                // 以当前节点的左子树去找到前驱节点删除
+                delete(bstNode.left, predecessor.key);
+
+                return new DeleteResult<K, V>(bstNode, delNode);
+            }
         }
     }
 
@@ -121,6 +187,21 @@ public class BSTMap<K extends Comparable<? super K>, V> implements Map16B<K, V> 
         }
     }
 
+    /**
+     * 删除
+     *
+     * @param key
+     * @return
+     */
+    public V remove(K key) {
+        var ret = BSTNode.delete(root, key);
+        root = ret.newNode;
+        if (ret.deletedNode == null) {
+            return null;
+        }
+        this.size -= 1;
+        return ret.deletedNode.value;
+    }
 
     /**
      * Returns whether this map contains a mapping for the specified key.
@@ -162,6 +243,38 @@ public class BSTMap<K extends Comparable<? super K>, V> implements Map16B<K, V> 
     @Override
     public Iterator<K> iterator() {
         return null;
+    }
+
+
+    public static void main(String[] args) {
+        var b = new BSTMap<String, Integer>();
+        b.put("d", 4);
+        b.put("b", 2);
+        b.put("f", 6);
+        b.put("a", 1);
+        b.put("c", 3);
+        b.put("e", 5);
+        b.put("g", 7);
+        var ret = new ArrayList<String>();
+        b.inorder(b.root, ret);
+        System.out.println(ret);
+        // 树的结构（完全平衡）：
+        //       d
+        //      / \
+        //     b   f
+        //    / \ / \
+        //   a  c e  g
+        b.remove("d");
+        ret = new ArrayList<String>();
+        b.inorder(b.root, ret);
+        System.out.println(ret);
+    }
+
+    private void inorder(BSTNode<K, V> node, List<K> keys) {
+        if (node == null) return;
+        inorder(node.left, keys);
+        keys.add(node.key);
+        inorder(node.right, keys);
     }
 
 }
