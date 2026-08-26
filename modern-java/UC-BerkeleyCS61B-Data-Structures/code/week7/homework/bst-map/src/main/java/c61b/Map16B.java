3a70d6aa84c33783b4ed9f7699c1addc7192c787
upangka <q10viking@outlook.com>
2026-08-26 15:02:23 +0800

    BSTMap - 创建核心方法

--- modern-java/UC-BerkeleyCS61B-Data-Structures/code/week7/homework/bst-map/src/main/java/c61b/Map16B.java
+++ modern-java/UC-BerkeleyCS61B-Data-Structures/code/week7/homework/bst-map/src/main/java/c61b/Map16B.java
@@ -7,4 +7,40 @@ package c61b;
  * @since 2026/8/26
  */
 public interface Map16B<K,V> extends Iterable<K> {
+
+    /**
+     * Associates the specified value with specified key in this map.
+     * If the map already contains the specified key,replace the key's mapping
+     * with the value specified.
+     * @param key
+     * @param value
+     */
+    void put(K key,V value);
+
+
+    /**
+     * Returns the value to which the specified key is mapped,or null if this
+     * map contains no mapping for the key
+     * @param key
+     * @return
+     */
+    V get(K key);
+
+    /**
+     * Returns whether this map contains a mapping for the specified key.
+     * @param key
+     * @return
+     */
+    boolean containsKey(K key);
+
+    /**
+     * Returns the number of key-value mappings in this map
+     * @return
+     */
+    int size();
+
+    /**
+     * Removes every mapping from this map
+     */
+    void clear();
 }
